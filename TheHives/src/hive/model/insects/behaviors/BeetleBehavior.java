aadcdeb68f57c03949b427cba63c6c9dff3bc2f7
lucas <lucas@lucas-k501lx.home>
2018-05-10 16:05:18 +0200

    ajout constructeur Cell

--- TheHives/src/hive/model/insects/behaviors/BeetleBehavior.java
+++ TheHives/src/hive/model/insects/behaviors/BeetleBehavior.java
@@ -6,10 +6,17 @@
 package hive.model.insects.behaviors;
 
 import hive.model.board.Cell;
+import hive.model.board.Cells;
+import hive.model.board.Tile;
+import hive.model.board.TilesStack;
 import hive.model.game.Game;
 import util.hexagons.Hexagon;
 import hive.model.insects.InsectBehavior;
 import java.util.ArrayList;
+import java.util.function.Predicate;
+import util.hexagons.iterators.NeighborsIterator;
+import util.hexagons.iterators.NeighborsValueIterator;
+import util.iterators.FilteringIterator;
 
 /**
  *
@@ -21,7 +28,33 @@ public class BeetleBehavior implements InsectBehavior
     @Override
     public ArrayList<Cell> getPossibleDestinations(Game game, Cell cell)
     {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        /*
+        ArrayList<Cell> list = new ArrayList<>();
+        
+        if(Cells.isConnex(cell, game.state.data.))
+        
+        NeighborsIterator<TilesStack> neighIter = new NeighborsIterator<>(cell.hexagon);
+
+        while (neighIter.hasNext()) //parcours des voisins du scarabe
+        {
+            Hexagon<TilesStack> currentHexagon = neighIter.next();
+            if (!currentHexagon.getValue().empty()) 
+            {
+                list.add(new Cell(currentHexagon)); //le scarabée peut "grimper" sur cette cellule, ne prend pas en compte la hauteur de l'esclade
+            } else
+            {
+                Tile scarabe = currentHexagon.getValue().pop();
+                NeighborsIterator neighbors = new NeighborsIterator<>(currentHexagon);
+                FilteringIterator<Hexagon<TilesStack>> fiter = new FilteringIterator<Hexagon<TilesStack>>(neighbors, n -> !n.getValue().isEmpty());
+
+                while (fiter.hasNext()) //itere sur les voisins non vide
+                {
+                    list.add(new Cell(fiter.next()));
+                }
+                currentHexagon.getValue().add(scarabe);
+            }
+        }
+        return list;*/
+        return null;
     }
-    
 }
