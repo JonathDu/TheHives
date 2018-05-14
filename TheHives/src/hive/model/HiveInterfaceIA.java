71f35618d9eff4f0caa12abbf630d27f58d7d03d
Coralie Durhone <coraliedurhone@gmail.com>
2018-05-14 11:11:00 +0200

    cas ou la queen n'est pas sur le plateau

--- TheHives/src/hive/model/HiveInterfaceIA.java
+++ TheHives/src/hive/model/HiveInterfaceIA.java
@@ -63,7 +63,8 @@ public class HiveInterfaceIA implements InterfaceIA
     public int queenFreeNeighbour(Player p, Game game)
     {
         HashSet<Cell> queen_positions = game.state.data.tiles.get(p.color).get(InsectType.QUEEN_BEE);
-        assert queen_positions.size() == 1;
+        if(queen_positions.isEmpty())
+            return 0;
         NeighborsIterator<TilesStack> neighIter = new NeighborsIterator<>(queen_positions.iterator().next().comb);
         int nbNeighbor = 0;
         while (neighIter.hasNext())
