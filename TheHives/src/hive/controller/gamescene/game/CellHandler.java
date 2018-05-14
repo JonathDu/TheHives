887336171a0ed9519b98a202eb1570748e67d18d
Thomas <Thomas@MSI>
2018-05-14 13:38:01 +0200

    + base controller + modif interfaceIA ???

--- TheHives/src/hive/controller/gamescene/game/CellHandler.java
+++ TheHives/src/hive/controller/gamescene/game/CellHandler.java
@@ -5,8 +5,10 @@
  */
 package hive.controller.gamescene.game;
 
-import hive.controller.gamescene.game.selectors.CellClickedSelector;
 import hive.model.board.Cell;
+import hive.model.players.actions.Action;
+import hive.model.players.decisions.Decision;
+import hive.model.players.decisions.HumanDecision;
 import javafx.event.EventHandler;
 import javafx.scene.input.MouseEvent;
 
@@ -19,7 +21,6 @@ public class CellHandler implements EventHandler<MouseEvent>
     GameController controller;
     Cell cell;
     
-    
     public CellHandler(GameController controller, Cell cell)
     {
         this.controller = controller;
@@ -31,7 +32,32 @@ public class CellHandler implements EventHandler<MouseEvent>
     {
         if(event.getEventType() == MouseEvent.MOUSE_CLICKED)
         {
-            controller.selection.accept(new CellClickedSelector(controller, event));
+            Decision decision = controller.progress.game.state.turn.getCurrent().decision;
+            if(decision instanceof HumanDecision)
+            {
+                HumanDecision human_decision = (HumanDecision)decision;
+                
+                switch(controller.builder.getState())
+                {
+                case BEGIN:
+                    controller.builder.setSource(cell);
+                case SOURCE_SELECTED:
+                    controller.builder.setDestination(cell); // TODO vérifier que c bien une autre cellule etc
+                    Action action = controller.builder.produce();
+                    human_decision.setAction(action);
+                    controller.progress.doAction();
+                    // TODO mettre à jour graphiquement source et destination (pas besoin de faire tout le plateau)
+                case TILE_SELECTED:
+                    controller.builder.setPlacement(cell);
+                    // TODO
+                }
+            }
+        }
+        // if MOUSE_MOVED (exemple)
+        else if(true)
+        {
+            // information about the tile ? IA or not
+            // etc
         }
     }
 }
